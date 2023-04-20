4d4b8cfc51b09479777bd767d85eedf385cf6917
voseji <vooseji@emgeresources.com>
2023-04-20 08:54:04 +0100

    pdf added

--- app/Http/Controllers/pages/Projects.php
+++ app/Http/Controllers/pages/Projects.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Dompdf\Dompdf;
+use Barryvdh\DomPDF\Facade;
 
 class Projects extends Controller
 {
