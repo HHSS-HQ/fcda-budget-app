751573c58b796b3e7008d5acafd406b13ed370fb
voseji <vooseji@emgeresources.com>
2023-05-23 11:30:54 +0100

    latest updates

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,31 +13,32 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('username')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->unsignedInteger('role_id')->nullable();
-            $table->rememberToken();
-            $table->timestamps();
+      Schema::create('users', function (Blueprint $table) {
+        $table->id();
+        $table->string('name', 191);
+        $table->char('email', 191)->unique();
+        $table->string('username', 191)->unique();
+        $table->timestamp('email_verified_at', 20)->nullable();
+        $table->string('password', 191);
+        $table->unsignedInteger('role_id', 20)->nullable();
+        $table->rememberToken();
+        $table->timestamps();
 
 
-            $table->index(["role_id"], 'fk_user_1_idx');
-            
+        $table->index(["role_id"], 'fk_user_1_idx');
 
 
-            $table->foreign('role_id', 'fk_user_1_idx')
-                ->references('id')->on('role')
-                ->onDelete('no action')
-                ->onUpdate('no action');
 
-        });
+        $table->foreign('role_id', 'fk_user_1_idx')
+            ->references('id')->on('role')
+            ->onDelete('no action')
+            ->onUpdate('no action');
+
+    });
+
     }
 
-    
+
 
     /**
      * Reverse the migrations.
--- database/migrations/2023_04_16_063519_contractor.php
+++ database/migrations/2023_04_16_063519_contractor.php
@@ -16,13 +16,13 @@ return new class extends Migration
       Schema::create('contractor', function(Blueprint $table){
         $table->engine = 'InnoDB';
         $table->increments('id');
-        $table->string('company_name')->unique();
-        $table->string('contractor_name')->nullable();
-        $table->string('contractor_account_number')->nullable();
-        $table->string('contractor_account_name')->nullable();
-        $table->string('contractor_bank')->nullable();
-        $table->string('contractor_phone_number')->nullable();
-        $table->string('alternate_phone_number')->nullable();
+        $table->string('company_name', 191)->unique();
+        $table->string('contractor_name', 191)->nullable();
+        $table->string('contractor_account_number', 191)->nullable();
+        $table->string('contractor_account_name', 191)->nullable();
+        $table->string('contractor_bank', 191)->nullable();
+        $table->string('contractor_phone_number', 191)->nullable();
+        $table->string('alternate_phone_number', 191)->nullable();
         $table->string('added_by')->references('id')->on('users')->nullable();
         $table->timestamps();
         $table->softDeletes();
