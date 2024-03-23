3e426530a7a54d2cdfd6aa27fe25efff54bc8fa3
voseji <vooseji@emgeresources.com>
2024-03-23 03:46:53 +0100

    w updates

---
+++ database/migrations/2024_02_04_044859_make_changes_to_payee_table2.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('project', function (Blueprint $table) {
+            $table->unsignedInteger('payee_id')->nullable();
+            $table->foreign('payee_id')
+         ->references('payee_id')->on('payee_new')
+         ->onDelete('no action')
+         ->onUpdate('no action');
+        });
+    }
+    
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        // Schema::table('users', function (Blueprint $table) {
+           
+        //         $table->unsignedBigInteger('department_id')->nullable();
+        // });
+
+        // Schema::table('project', function (Blueprint $table) {
+        //     Schema::dropColumn('payee_id');
+        // });
+    }
+};
